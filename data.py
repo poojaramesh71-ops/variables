e2108a9dc383369d677bd2527f30b1ac4829e8cf
Pooja Ramesh <kumbarp71@gmail.com>
2026-01-08 13:06:45 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,10 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Quotient:", num1 / num2)
